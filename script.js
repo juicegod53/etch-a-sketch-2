482647b5e1d6c654c687d85c53efc14d88397d73
Abhi Kalakoti <abhikalakoti@gmail.com>
2025-04-11 12:29:48 +0100

    Random colour and opacity increases each time

--- script.js
+++ script.js
@@ -13,11 +13,18 @@ button.addEventListener("click", function() {
     new_grid_squares = parseInt(new_grid_squares)
     if (new_grid_squares <= 100) {
         container.innerHTML = ''
-        generate_grid(new_grid_squares)
+        generateGrid(new_grid_squares)
     }
 })
 
-function generate_grid(squares) {
+function random_rgb() {
+    r = Math.floor(Math.random() * 255)
+    g = Math.floor(Math.random() * 255)
+    b = Math.floor(Math.random() * 255)
+    return 'rgb(' + r + ',' + g + ',' + b + ',0.1)'
+}
+
+function generateGrid(squares) {
     for (let i = 0; i < squares; i++) {
         let div = document.createElement("div")
         div.style.display = "flex"
@@ -25,8 +32,18 @@ function generate_grid(squares) {
         for (let j = 0; j < squares; j++) {
             let subdiv = document.createElement("div")
             subdiv.style.border = "2px solid black"
+            subdiv.style.backgroundColor = "white"
             subdiv.addEventListener("mouseover", function(e) {
-                e.target.style.backgroundColor = "black"
+                randomColour = random_rgb()
+                closingBracket = e.target.style.backgroundColor.indexOf(')')
+                currentOpacity = e.target.style.backgroundColor.substring(closingBracket-3,closingBracket)
+                currentOpacity = parseFloat(currentOpacity)
+                if (currentOpacity < 1) {
+                    newOpacity = currentOpacity + 0.1
+                    randomColour = e.target.style.backgroundColor.substring(0,closingBracket-3) + parseFloat(newOpacity) + ')'
+                }
+                if (currentOpacity < 1 || subdiv.style.backgroundColor == 'white')
+                e.target.style.backgroundColor = randomColour
             })
             if (i != (squares-1)) {
                 subdiv.style.borderBottom = "0"
@@ -42,5 +59,5 @@ function generate_grid(squares) {
     }
 }
 
-generate_grid(default_squares)
+generateGrid(default_squares)
 
